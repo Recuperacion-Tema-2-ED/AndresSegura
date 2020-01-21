a8dc0b86663b8260343ad7868ad30b1bdf4bc430
Andres Segura <andres14.ass@gmail.com>
2020-01-21 11:01:02 +0100

    autor añadido a Coche.java

---
+++ Coche.java
@@ -0,0 +1,13 @@
+public class Coche {
+    private String modelo;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
+
+// Andrés Segura Sáez
